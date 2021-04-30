de41d2d1c910c61b96b477764f1a956aa94331de
Roman19933 <mikhailiuk.roman@gmail.com>
2021-04-30 14:50:28 +0300

    refactor: clear and optimization code, add loader and style to loader and fix style

--- src/lib/spinner.js
+++ src/lib/spinner.js
@@ -1,17 +1,11 @@
 export default class spinner {
 
-  /**
-* add spinners
-*/
   addSpinner() {
     let el = document.querySelector('.fm-wrapper');
     let html = `<div class="fm-spinner"><div class="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div></div>`
     el.insertAdjacentHTML('afterbegin', html)
   }
 
-  /**
-* remove spinners
-*/
   removeSpinner() {
     let el = document.querySelector('.fm-spinner');
     el.remove()
